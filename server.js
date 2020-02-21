61b56af3294dc9818164cc15e900bcd8404c0b09
Sporego <Hello@Filbert.me>
2020-02-21 02:30:10 -0500

    works

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ async function scrapeProduct(url) {
   const page = await browser.newPage();
   await page.goto(url);
 
-  const [el] = page.$x('//*[@id="lid-464619280"]/div/div/div[2]/h2/a/span');
+  const [el] = await page.$x(
+    "/html/body/div[2]/div/div[1]/div[1]/div[2]/div[2]/div[1]/div/div/div[2]/h2/a/span"
+  );
   const src = await el.getProperty("src");
   const srcTxt = await src.jsonValue();
 
-  console.log({srcTxt});
+  console.log({ srcTxt });
 }
 
-scrapeProduct('https://www.yellowpages.com/search?search_terms=cpa&geo_location_terms=New%20York%2C%20NY&page=1').catch(() => {});
+scrapeProduct(
+  "https://www.yellowpages.com/search?search_terms=cpa&geo_location_terms=New%20York%2C%20NY&page=1"
+);
